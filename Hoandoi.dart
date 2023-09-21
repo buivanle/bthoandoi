2143ce069faa3543cbbcd1da5d875904d28c406a
buivanle <92353236+buivanle@users.noreply.github.com>
2023-09-21 14:17:38 +0700

    Add files via upload

---
+++ Hoandoi.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  final phones = <String>['Nokia', 'Xiaomi', 'iPhone'];
+
+  print(phones);
+  print(
+      '${identityHashCode(phones[0])} - ${identityHashCode(phones[1])} - ${identityHashCode(phones[2])}');
+
+  // viết code ở đây, đổi vị trí Xiaomi và iPhone
+  // var temp = phones[1];
+  // phones[1] = temp;
+  // phones[2] = 'Xiaomi';
+
+  //dùng biến tạm
+  var temp = phones[1];
+  phones[1] = phones[2];
+  phones[2] = temp;
+
+
+
+  print(phones);
+  print(
+      '${identityHashCode(phones[0])} - ${identityHashCode(phones[1])} - ${identityHashCode(phones[2])}');
+}
